cf8c887e39a5e4f36defa1c7c3a44f6266c8f0b9
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 21:34:07 +0300

    Add formatDate function for date formatting

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,20 @@
+function formatDate(
+  date: string | Date
+){
+
+  const newDate = new Date(date);
+
+
+  return newDate.toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    }
+  );
+
+}
+
+
+export default formatDate;
