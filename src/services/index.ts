48c59fd998cce434b8ed20553b0260aca374ae8d
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 21:45:24 +0300

    Create index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,19 @@
+export * from "./authService";
+
+export * from "./userService";
+
+export * from "./projectService";
+
+export * from "./courseService";
+
+export * from "./paymentService";
+
+export * from "./notificationService";
+
+export * from "./uploadService";
+
+export * from "./searchService";
+
+export * from "./contactService";
+
+export * from "./settingsService";
