557a2e97501d9b5def6fd55dfea3370aed04f160
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 21:25:15 +0300

    Update useLanguage.ts

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,36 +1,22 @@
 import { useContext } from "react";
-
-import {
-  LanguageContext
-} from "../context/LanguageContext";
-
+import { LanguageContext } from "../context/LanguageContext";
 
 
 const useLanguage = () => {
 
-
-  const context =
-    useContext(LanguageContext);
-
+  const context = useContext(LanguageContext);
 
 
   if (!context) {
-
-
     throw new Error(
       "useLanguage must be used within a LanguageProvider"
     );
-
-
   }
-
 
 
   return context;
 
-
 };
-
 
 
 export default useLanguage;
