0120ed34b1c56fbf591eed77146d11f506bafd9b
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 19:04:28 +0300

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import Hero from "../components/sections/Hero";
+import Features from "../components/sections/Features";
+import Services from "../components/sections/Services";
+import Pricing from "../components/sections/Pricing";
+import Portfolio from "../components/sections/Portfolio";
+import Testimonials from "../components/sections/Testimonials";
+import FAQ from "../components/sections/FAQ";
+import CTA from "../components/sections/CTA";
+import Contact from "../components/sections/Contact";
+
+
+function Home() {
+
+
+  return (
+
+    <>
+
+      <Hero />
+
+
+      <Features />
+
+
+      <Services />
+
+
+      <Pricing />
+
+
+      <Portfolio />
+
+
+      <Testimonials />
+
+
+      <FAQ />
+
+
+      <CTA />
+
+
+      <Contact />
+
+
+    </>
+
+  );
+
+}
+
+
+export default Home;
