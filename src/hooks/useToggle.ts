5030a3aec717111bc9d5627fe69d1fc09e8fc764
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 21:32:56 +0300

    Create useToggle.ts

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,68 @@
+import {
+  useState
+} from "react";
+
+
+
+function useToggle(
+
+initialValue:boolean = false
+
+){
+
+
+const [value,setValue] =
+useState<boolean>(initialValue);
+
+
+
+
+
+const toggle = () => {
+
+  setValue(prev => !prev);
+
+};
+
+
+
+
+
+const setTrue = () => {
+
+  setValue(true);
+
+};
+
+
+
+
+
+const setFalse = () => {
+
+  setValue(false);
+
+};
+
+
+
+
+
+return {
+
+value,
+
+toggle,
+
+setTrue,
+
+setFalse
+
+};
+
+
+}
+
+
+
+export default useToggle;
