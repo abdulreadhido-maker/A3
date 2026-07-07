9c7f333c9e02bd3682c292726447d50dcdae6ad3
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-07 21:38:18 +0300

    Create index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,35 @@
+export { default as formatDate }
+from "./formatDate";
+
+
+export { default as formatCurrency }
+from "./formatCurrency";
+
+
+export { default as validateEmail }
+from "./validateEmail";
+
+
+export { default as cn }
+from "./cn";
+
+
+export {
+  setStorage,
+  getStorage,
+  removeStorage
+}
+from "./storage";
+
+
+export * from "./constants";
+
+
+export * from "./helpers";
+
+
+export { default as hasPermission }
+from "./permissions";
+
+
+export * from "./number";
