d9ae223977dbc0a01b2d95f2fe849704886d9e78
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:25:47 +0300

    Create index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+// Images
+export { default as Logo } from "./images/logo.png";
+export { default as HeroImage } from "./images/hero.png";
+export { default as AboutImage } from "./images/about.png";
+export { default as PlaceholderImage } from "./images/placeholder.png";
+
+
+
+// Icons
+export { default as Favicon } from "./icons/favicon.svg";
+
+
+
+// Videos
+export { default as HeroVideo } from "./videos/hero.mp4";
