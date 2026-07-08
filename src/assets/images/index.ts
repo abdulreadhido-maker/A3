67f6770f0ef2f5c7c508e23ac6468d410e910b3c
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:26:11 +0300

    Create index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+export { default as Logo } from "./logo.png";
+
+export { default as Hero } from "./hero.png";
+
+export { default as About } from "./about.png";
+
+export { default as Services } from "./services.png";
+
+export { default as Portfolio } from "./portfolio.png";
+
+export { default as Courses } from "./courses.png";
+
+export { default as Contact } from "./contact.png";
+
+export { default as Placeholder } from "./placeholder.png";
