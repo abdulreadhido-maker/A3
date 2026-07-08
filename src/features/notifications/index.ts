8cf2492e89c94091c8262c7f129849d0f94bfcb6
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 16:58:05 +0300

    Create index.ts

---
+++ src/features/notifications/index.ts
@@ -0,0 +1,11 @@
+export * from "./hooks";
+
+export * from "./services";
+
+export * from "./types";
+
+export { default as NotificationCard }
+from "./components/NotificationCard";
+
+export { default as NotificationList }
+from "./components/NotificationList";
