d923bbc7b72646be996bbac5e2deed816c5aab20
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:32:54 +0300

    Create index.ts

---
+++ src/features/auth/index.ts
@@ -0,0 +1,8 @@
+export * from "./hooks";
+export * from "./services";
+export * from "./types";
+
+export { default as LoginForm } from "./components/LoginForm";
+export { default as RegisterForm } from "./components/RegisterForm";
+export { default as ForgotPasswordForm } from "./components/ForgotPasswordForm";
+export { default as ResetPasswordForm } from "./components/ResetPasswordForm";
