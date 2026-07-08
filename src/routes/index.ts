1b48d3102effd6f585aef641c79f67e542c0fae6
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:16:34 +0300

    Create index.ts

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export { default as AppRouter } from "./AppRouter";
+
+export { default as ProtectedRoute } from "./ProtectedRoute";
+
+export { default as PublicRoute } from "./PublicRoute";
+
+export { default as RouteLoader } from "./RouteLoader";
