8f596cffa5d8da8af86128020612d034ac7fe903
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:45:10 +0300

    Create index.ts

---
+++ src/features/courses/index.ts
@@ -0,0 +1,17 @@
+export * from "./hooks";
+
+export * from "./services";
+
+export * from "./types";
+
+
+export { default as CourseCard }
+from "./components/CourseCard";
+
+
+export { default as CourseList }
+from "./components/CourseList";
+
+
+export { default as CourseForm }
+from "./components/CourseForm";
