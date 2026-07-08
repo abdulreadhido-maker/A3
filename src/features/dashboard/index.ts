5942093bc5fb4fb6834d8a8db61f507152caada9
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:36:13 +0300

    Create index.ts

---
+++ src/features/dashboard/index.ts
@@ -0,0 +1,8 @@
+export * from "./hooks";
+export * from "./services";
+export * from "./types";
+
+export { default as DashboardStats } from "./components/DashboardStats";
+export { default as RecentProjects } from "./components/RecentProjects";
+export { default as RecentNotifications } from "./components/RecentNotifications";
+export { default as QuickActions } from "./components/QuickActions";
