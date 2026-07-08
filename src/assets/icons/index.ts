646d900e5e851bf259ae42ad1fa8e8aa2ceab98f
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:27:02 +0300

    Create index.ts

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,15 @@
+export { default as Favicon } from "./favicon.svg";
+
+export { default as LogoIcon } from "./logo.svg";
+
+export { default as MenuIcon } from "./menu.svg";
+
+export { default as CloseIcon } from "./close.svg";
+
+export { default as SearchIcon } from "./search.svg";
+
+export { default as UserIcon } from "./user.svg";
+
+export { default as DashboardIcon } from "./dashboard.svg";
+
+export { default as SettingsIcon } from "./settings.svg";
