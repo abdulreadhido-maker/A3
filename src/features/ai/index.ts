09015386916306414f17622dcb5071b239833803
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 17:01:35 +0300

    Create index.ts

---
+++ src/features/ai/index.ts
@@ -0,0 +1,17 @@
+export * from "./hooks";
+
+export * from "./services";
+
+export * from "./types";
+
+export { default as ChatBox }
+from "./components/ChatBox";
+
+export { default as ChatInput }
+from "./components/ChatInput";
+
+export { default as ChatMessage }
+from "./components/ChatMessage";
+
+export { default as ChatHistory }
+from "./components/ChatHistory";
