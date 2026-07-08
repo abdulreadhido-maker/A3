80a475476e5c3bad451e563b208f159b8c025f2a
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:49:21 +0300

    Create index.ts

---
+++ src/features/shop/index.ts
@@ -0,0 +1,25 @@
+export * from "./hooks";
+
+export * from "./services";
+
+export * from "./types";
+
+
+export { default as ProductCard }
+from "./components/ProductCard";
+
+
+export { default as ProductList }
+from "./components/ProductList";
+
+
+export { default as ProductForm }
+from "./components/ProductForm";
+
+
+export { default as Cart }
+from "./components/Cart";
+
+
+export { default as Checkout }
+from "./components/Checkout";
