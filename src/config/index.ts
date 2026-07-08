8eda0067717794e4d527dff45d1aba5c96b62839
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:14:37 +0300

    Create index.ts

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export { appConfig } from "./appConfig";
+
+export { routesConfig } from "./routesConfig";
+
+export { themeConfig } from "./themeConfig";
+
+export { i18nConfig } from "./i18nConfig";
+
+export { apiConfig } from "./apiConfig";
