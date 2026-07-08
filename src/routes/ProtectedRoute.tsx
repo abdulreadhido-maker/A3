3316be0dc6fe3067181cb4d8f3861d82d954ee16
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:15:27 +0300

    Create ProtectedRoute.tsx

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,40 @@
+import { Navigate } from "react-router-dom";
+import { ReactNode } from "react";
+
+import useAuth from "../hooks/useAuth";
+
+
+
+interface ProtectedRouteProps {
+
+  children: ReactNode;
+
+}
+
+
+
+function ProtectedRoute({
+
+  children
+
+}: ProtectedRouteProps) {
+
+  const { isAuthenticated } = useAuth();
+
+
+
+  if (!isAuthenticated) {
+
+    return <Navigate to="/login" replace />;
+
+  }
+
+
+
+  return <>{children}</>;
+
+}
+
+
+
+export default ProtectedRoute;
