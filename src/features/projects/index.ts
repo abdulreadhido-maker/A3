1ff7b3446465c074c470e1c7efddf6eebaad8003
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-08 11:40:24 +0300

    Create index.ts

---
+++ src/features/projects/index.ts
@@ -0,0 +1,17 @@
+export * from "./hooks";
+
+export * from "./services";
+
+export * from "./types";
+
+
+export { default as ProjectCard }
+from "./components/ProjectCard";
+
+
+export { default as ProjectList }
+from "./components/ProjectList";
+
+
+export { default as ProjectForm }
+from "./components/ProjectForm";
